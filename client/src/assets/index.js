3ed2bcc905c8c378387a429ae9df579d0d6e65ec
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-11-02 15:45:15 -0500

    added party popper image

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,3 +2,4 @@ export { default as Logo } from "./logo_no_bg.png";
 export { default as LogoName } from "./full_logo_w.png";
 export { default as GoogleLogo } from "./google-logo.png";
 export { default as TMDBLogo } from "./tmdb.svg";
+export { default as PartyPopper } from "./party.png";
