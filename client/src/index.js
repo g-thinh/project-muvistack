2a7cdea60fa94306131e5ccd49a631641e5e424f
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-11-03 09:13:10 -0500

    added redux store actions/reducers to app

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import AuthProvider from "./components/AuthContext";
 import AvatarProvider from "./components/AvatarContext";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+
+const store = configureStore();
 
 ReactDOM.render(
   <AuthProvider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </AuthProvider>,
+
   document.getElementById("root")
 );
---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import TOGGLERS from "./ui-reducer";
+
+export default combineReducers({ TOGGLERS });
