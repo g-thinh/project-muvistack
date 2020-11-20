4e57960a9bf1cbaac220468dc1828903fe616bbc
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-11-19 22:23:38 -0500

    added build

---
+++ client/build/precache-manifest.cce15e7536312dbf4c7a3f186a3dd943.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af91ad726951b81b9acecc9bc48a5dd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ce1551096ba8ca7c217",
+    "url": "/static/css/2.2fc29f82.chunk.css"
+  },
+  {
+    "revision": "9ce1551096ba8ca7c217",
+    "url": "/static/js/2.46016832.chunk.js"
+  },
+  {
+    "revision": "3c0b69cc4c0ca623a63f916ca9561f84",
+    "url": "/static/js/2.46016832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ffe198c1323b5c2a0e",
+    "url": "/static/js/main.ae8a7e10.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "713f5bb37518055fe5d61a18dd7cb232",
+    "url": "/static/media/cry.713f5bb3.png"
+  },
+  {
+    "revision": "ecc844337bfe607020e2aa4aa3562e29",
+    "url": "/static/media/google-logo.ecc84433.png"
+  },
+  {
+    "revision": "2172f7894aad338812ad33eae31da48f",
+    "url": "/static/media/logo_no_bg.2172f789.png"
+  },
+  {
+    "revision": "87f081ea16e90c001987bd6ca344a8b2",
+    "url": "/static/media/party.87f081ea.png"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/static/media/tmdb.56fd61ee.svg"
+  }
+]);
